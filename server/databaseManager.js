bac332935574ee5243cfcfaa8ab07bf28ddfd432
Paulobergine <pguerreftb@gmail.com>
2023-12-19 20:26:53 -0500

    Add several http request

--- server/databaseManager.js
+++ server/databaseManager.js
@@ -11,6 +11,9 @@ const connection = mysql.createConnection({
   database: 'sukify'
 });
 
+/**
+ * Connect the database
+ */
 const connectDatabase = () => {
   connection.connect((err) => {
     if (err) {
@@ -21,33 +24,131 @@ const connectDatabase = () => {
   });
 };
 
-const printMusics = () => {
-  connection.query('SELECT * FROM Musics', (err, results, fields) => {
-    if (err) {
-      console.error('Error retrieving data:', err);
-      return;
-    }
+/**
+ * Insert a new music
+ */
+const insertMusic = ({ musicID, musicTitle, musicDuration }) => {
+  return new Promise((resolve, reject) => {
+    const query = 'INSERT INTO Music (musicID, musicTitle, musicDuration) VALUES (?, ?, ?)';
+    const values = [musicID, musicTitle, musicDuration];
 
-    console.log('All records in the Musics table:');
-    console.table(results);
+    connection.query(query, values, (err, results, fields) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(results); 
+    });
   });
 };
 
-const insertMusic = ({ musicID, musicTitle, musicDuration }) => {
-  const query = 'INSERT INTO Musics (musicID, musicTitle, musicDuration) VALUES (?, ?, ?)';
-  const values = [musicID, musicTitle, musicDuration];
+/**
+ * Insert a new playlist
+ */
+const insertPlaylist = ({ playlistName, thumbnail }) => {
+  return new Promise((resolve, reject) => {
+    const query = 'INSERT INTO Playlist (name, thumbnailPath) VALUES (?, ?)';
+    const values = [playlistName, thumbnail];
 
-  connection.query(query, values, (err, results, fields) => {
-    if (err) {
-      console.log('Error inserting data:', err);
-      return;
-    }
-
-    console.log('Data inserted');
-    printMusics();
-  })
+    connection.query(query, values, (err, results, fields) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(results);
+    });
+  });
 };
 
+/**
+ * Update a specific playlist
+ */
+const updatePlaylist = ({ playlistID, newName, newThumbnail }) => {
+  return new Promise((resolve, reject) => {
+    let query = 'UPDATE Playlist SET';
+    const values = [];
+
+    if (newName) {
+      query += ' name = ?,';
+      values.push(newName);
+    }
+
+    if (newThumbnail) {
+      query += ' thumbnailPath = ?,';
+      values.push(newThumbnail);
+    }
+
+    query = query.slice(0, -1);
+    query += ' WHERE id = ?';
+    values.push(playlistID);
+
+    connection.query(query, values, (err, results, fields) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(results);
+    });
+  });
+};
+
+/**
+ * Delete a specific playlist
+ */
+const deletePlaylist = (playlistID) => {
+  return new Promise((resolve, reject) => {
+    const query = 'DELETE FROM Playlist WHERE id = ?';
+    const values = [playlistID];
+
+    connection.query(query, values, (err, results, fields) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(results);
+    });
+  });
+};
+
+/**
+ * Delete specific music
+ */
+const deleteMusic = (musicID) => {
+  return new Promise((resolve, reject) => {
+    const query = 'DELETE FROM Music WHERE id = ?';
+    const values = [musicID];
+
+    connection.query(query, values, (err, results, fields) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(results);
+    });
+  });
+};
+
+/**
+ * Update a specific music
+ */
+const updateMusic = ({ musicID, musicTitle }) => {
+  return new Promise((resolve, reject) => {
+    const query = 'UPDATE Music SET musicTitle = ? WHERE id = ?';
+    const values = [ musicTitle, musicID ];
+    
+    connection.query(query, values, (err, results, fields) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(results);
+    }); 
+  });
+};
+
+/**
+ * End the connection
+ */
 const endDatabase = () => {
   connection.end((err) => {
     if (err) {
@@ -58,4 +159,4 @@ const endDatabase = () => {
   });
 };
 
-module.exports = {connectDatabase, endDatabase, insertMusic, printMusics};
+module.exports = {connectDatabase, endDatabase, insertMusic, insertPlaylist, updatePlaylist, deletePlaylist, deleteMusic, updateMusic};
--- server/server.js
+++ server/server.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const cors = require('cors');
+const multer = require('multer');
 
 const { downloadMP3, searchVideoByName, getMusicDuration } = require('./ytb-scraper');
-const { connectDatabase, insertMusic, printMusics } = require('./databaseManager');
+const { connectDatabase, insertMusic, insertPlaylist, updatePlaylist, deletePlaylist, deleteMusic, updateMusic } = require('./databaseManager');
+
+require('dotenv').config({ path: 'credentials.env' });
+const thumbnailFolder = process.env.THUMBNAILS_FOLDER;
 
 const app = express();
 const port = 7000;
 
 connectDatabase();
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, thumbnailFolder);
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    }
+});
+  
+const upload = multer({ storage: storage });
 
 /**
  * CORS
@@ -24,25 +39,149 @@ app.use(cors({
 app.post('/download', (req, res) => {
     const videoName = req.query.videoName;
 
+    if (!videoName) {
+        console.log('Error : missing parameters');
+        return res.status(500).send('Missing parameters');
+    }
+
     searchVideoByName(videoName).then(({ videoID, videoTitle }) => {
         const music = { musicID: videoID, musicTitle: videoTitle };
-
-        // TODO : check if the videoID is not already in the database
 
         downloadMP3(videoID).then(() => {
             getMusicDuration(videoID).then((duration) => {
                 music.musicDuration = duration;
-                insertMusic(music);
-                console.log('Video downloaded as MP3 successfully!');
-                res.status(200).send('Video downloaded as MP3 successfully!');
+
+                insertMusic(music).then(() => {
+                    console.log('Video downloaded as MP3 successfully');
+                    return res.status(200).send('Video downloaded as MP3 successfully');
+                }).catch((err) => {
+                    console.log('Error: ' + err);
+                    return res.status(500).send('Video already downloaded');
+                });
             });
         }).catch((err) => {
             console.log('Error: ' + err);
-            res.status(500).send('Error: ' + err);
-        })
+            return res.status(500).send('Error while downloading the music');
+        });
     }).catch((err) => {
         console.log('Error: ' + err);
-        res.status(500).send('Error');
+        return res.status(500).send('Error while searching the music');
+    });
+});
+
+/**
+ * Create a playlist
+ */
+app.post('/createPlaylist', upload.single('thumbnail'), (req, res) => {
+    const playlist = { playlistName: req.query.playlistName, thumbnail: req.file?.originalname };
+
+    if (!playlist.playlistName || !playlist.thumbnail) {
+        console.log('Error : missing parameters');
+        return res.status(500).send('Missing parameters');
+    }
+
+    insertPlaylist(playlist).then(() => {
+        console.log('Playlist successfully created');
+        return res.status(200).send("Playlist successfully creayed");
+    }).catch((err) => {
+        console.log('Error: ' + err);
+        return res.status(500).send('Error while creating playlist');
+    });
+});
+
+/**
+ * Update either the name or the thumbnail of the playlist, or both
+ */
+app.put('/updatePlaylist/:playlistID', upload.single('thumbnail'), (req, res) => {
+    const newPlaylist = { playlistID: req.params.playlistID, newName: req.query.newName, newThumbnail: req.file?.originalname };
+
+    if (!newPlaylist.playlistID || (!newPlaylist.newName && !newPlaylist.newThumbnail)) {
+        console.log('Error : missing parameters');
+        return res.status(500).send('Missing parameters');
+    }
+
+    updatePlaylist(newPlaylist).then((results) => {
+        if (results.affectedRows) {
+            console.log('Playlist updated successfully');
+            return res.status(200).send('Playlist updated successfully');
+        }
+        console.log('The playlist does not exist');
+        return res.status(200).send('The playlist does not exist');
+    }).catch((err) => {
+        console.log('Error:' + err);
+        return res.status(500).send('Error while updating playlist');
+    });
+});
+
+/**
+ * Delete a playlist
+ */
+app.delete('/deletePlaylist/:playlistID', (req, res) => {
+    const playlistID = req.params.playlistID;
+
+    if (!playlistID) {
+        console.log('Error : missing parameters');
+        return res.status(500).send('Missing parameters');
+    }
+
+    deletePlaylist(playlistID).then((results) => {
+        if (results.affectedRows) {
+            console.log('Playlist successfully deleted');
+            return res.status(200).send('Playlist successfully deleted');
+        }
+        console.log('The playlist does not exist');
+        return res.send('The playlist does not exist');
+    }).catch((err) => {
+        console.log('Error :' + err);
+        return res.status(500).send('Error while deleting playlist');
+    });
+});
+
+/**
+ * Delete a music
+ */
+app.delete('/deleteMusic/:musicID', (req, res) => {
+    const musicID = req.params.musicID;
+
+    if (!musicID) {
+        console.log('Error : missing parameters');
+        return res.status(500).send('Missing parameters');
+    }
+
+    deleteMusic(musicID).then((results) => {
+        if (results.affectedRows) {
+            console.log('Music successfully deleted');
+            return res.status(200).send('Music successfully deleted');
+        }
+        console.log('The music does not exist');
+        return res.status(200).send('The music does not exist');
+    }).catch((err) => {
+        console.log('Error :' + err);
+        return res.status(500).send('Error while deleting music');
+    });
+});
+
+/**
+ * Update a music
+ */
+app.put('/updateMusic/:musicID', (req, res) => {
+    const music = { musicID: req.params.musicID, musicTitle: req.query.newName };
+
+    if (!music.musicID || !music.musicTitle) {
+        console.log('Error : missing parameters');
+        return res.status(500).send('Missing parameters');
+    }
+
+    updateMusic(music).then((results) => {
+        if (results.affectedRows) {
+            console.log('Music updated successfully');
+            return res.status(200).send('Music updated successfully');
+        }
+        console.log('The music does not exist');
+        return res.status(500).send('The music does not exist');
+    }).catch((err) => {
+        console.log('Error : ' + err);
+        return res.status(500).send('Error while updating music');
     });
 });
 
