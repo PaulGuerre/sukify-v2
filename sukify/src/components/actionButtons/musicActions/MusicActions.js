c5dee19bcde1664ae6265a4c8ab6e2d84a916d4e
Paulobergine <pguerreftb@gmail.com>
2024-01-25 16:55:01 -0500

    added musicActions component

---
+++ sukify/src/components/actionButtons/musicActions/MusicActions.js
@@ -0,0 +1,24 @@
+'use client'
+
+import Image from 'next/image';
+import styles from './MusicActions.module.css';
+import dots from '@/lib/icons/dots.svg';
+import { useState } from 'react';
+
+export default function MusicActions() {
+    const [ isVisible, setIsVisible ] = useState(false);
+
+    return (
+        <div className={styles.test}>
+            <button className={styles.button} onClick={() => setIsVisible(!isVisible)}><Image src={dots} alt="three dots icon" /></button>
+            {
+                isVisible &&
+                <div className={styles.menu}>
+                    <button className={styles.menuAction}>Add</button>
+                    <button className={styles.menuAction}>Edit</button>
+                    <button className={styles.menuAction}>Delete</button>
+                </div>
+            }
+        </div>
+    );
+}
