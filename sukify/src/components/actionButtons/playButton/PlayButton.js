8b578c77be69a7a92711d5a515cff70eb6196e69
Paulobergine <pguerreftb@gmail.com>
2024-01-25 16:55:17 -0500

    created a single playButton component

---
+++ sukify/src/components/actionButtons/playButton/PlayButton.js
@@ -0,0 +1,15 @@
+'use client'
+
+import Image from 'next/image';
+import styles from './PlayButton.module.css';
+import playGrey from '@/lib/icons/play_grey.svg';
+import playDark from '@/lib/icons/play_dark.svg';
+import { useState } from 'react';
+
+export default function PlayButton() {
+    const [ iconIdHovered, setIconIdHovered ] = useState(false);  
+
+    return (
+        <button className={styles.button} onMouseEnter={() => setIconIdHovered(true)} onMouseLeave={() => setIconIdHovered(false)}><Image src={iconIdHovered ? playGrey : playDark} alt="play icon"/></button>
+    );
+}
