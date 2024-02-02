73b21b9cf80929538a745fda10ff5e1e6a8ba568
Paulobergine <pguerreftb@gmail.com>
2024-02-02 17:30:18 -0500

    style update x2

--- sukify/src/components/customAlert/CustomAlert.js
+++ sukify/src/components/customAlert/CustomAlert.js
@@ -1,8 +1,12 @@
 import styles from './CustomAlert.module.css';
 
 export default function CustomAlert({ data }) {
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') data.firstAction();
+    }
+
     return (
-        <div className={styles.alert}>
+        <div className={styles.alert} onKeyDown={handleKeyDown}>
             <div className={styles.main}>
                 { data.form }
                 <div className={styles.actions}>
