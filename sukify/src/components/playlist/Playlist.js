5f383589df87251ea23dc001005797de23c51873
Paulobergine <pguerreftb@gmail.com>
2024-02-05 18:23:53 -0500

    fix playlist display

--- sukify/src/components/playlist/Playlist.js
+++ sukify/src/components/playlist/Playlist.js
@@ -7,10 +7,10 @@ export default function Playlist({ playlist }) {
     return (
         <Link href={`/playlists/${playlist.id}`} className={`${styles.playlist} ${styles[`cover-${Math.floor(Math.random() * 4) + 1}`]}`}>
             <div className={styles.playlistName}>{playlist.name}</div>
-            <div className={styles.playlistLength}>{playlist.MusicCount} musics</div>
             <div className={styles.play}>
                 <Image src={play} alt="play icon" />
             </div>
+            <div className={styles.playlistLength}>{playlist.MusicCount} musics</div>
         </Link>
     )
 }
